50a7b25468ffd92685c9e248cf3d9164816bcd28
ajinasokan <ajinpanthayil@gmail.com>
2020-03-09 12:22:57 +0530

    compile for host arch, add fPIC

--- standard_definitions.mk
+++ standard_definitions.mk
@@ -4,7 +4,7 @@ CC=/usr/bin/gcc
 #CC=/opt/musl/bin/musl-gcc
 
 #compilerflags
-COMPFLAGS=-pipe -Os -mtune=i386 -Wall -D_XOPEN_SOURCE=600 -D_BSD_SOURCE -D_GNU_SOURCE -fno-strength-reduce -nodefaultlibs -fno-strict-aliasing  -I. -ffunction-sections -fdata-sections
+COMPFLAGS=-fPIC -pipe -Os -Wall -D_XOPEN_SOURCE=600 -D_BSD_SOURCE -D_GNU_SOURCE -fno-strength-reduce -nodefaultlibs -fno-strict-aliasing  -I. -ffunction-sections -fdata-sections
 
 COMMONDEFS=$(COMPFLAGS) -D_BSD_SOURCE -D_GNU_SOURCE -DUSECMS
 
